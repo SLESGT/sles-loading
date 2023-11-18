046e74129a141cc2f3989aa4069d8a0e0366e254
Ashan Hannadige <151367115+SLESGT@users.noreply.github.com>
2023-11-18 17:06:09 +0530

    Add files via upload

---
+++ assets/scripts/config.js
@@ -0,0 +1,41 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "YOUR IP SERVER";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Click here to copy the link and join our Discord server!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/zHsrqHcuG2"},
+    {name: "facebook", label: "Facebook", description: "SLES RP Official Facebook Page feel free to join and follow us!", icon: "assets/media/icons/tiktok.png", link: "https://web.facebook.com/profile.php?id=100094010988231"},
+    {name: "youtube", label: "YouTube", description: "SLES RP Official YouTube Channel", icon: "assets/media/icons/tebex.png", link: "https://www.youtube.com/channel/UCUVp5ovIKYpCCjB8vsJKm9Q"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "GT", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1174350241714147349/1175344778607599716/IMG_2866.jpg?ex=656ae420&is=65586f20&hm=b8d972fbe4105e92132567a4ac2f91c92ccb9af85134de1ad9fd67496c9c136c&"},
+    {name: "HUNTER", description: "Chief Admin", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1173488210542874624/1175349962729607198/IMG_8523.png?ex=656ae8f4&is=655873f4&hm=81d2065ad34952386efc86333728e91ef9b3184e924b715bb1e89cddaf95ae16&"},
+    {name: "SHESHAN", description: "Chief Admin", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1174350241714147349/1175348426037280839/315705912_3230361103960267_4362374922529351047_n.jpg?ex=656ae785&is=65587285&hm=10600912bbffd5346d20f34c5a1417c1b53d7aef9eb568c54e3b279ddd02f7cd&"},
+    {name: "JANIYA", description: "Chief Admin", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1173488210542874624/1175349927698776134/IMG_8525.jpg?ex=656ae8eb&is=655873eb&hm=8cf6cb4c37015b9b9725630a2f488929ba620621882312a031fcff79cfbf50ae&"},
+    {name: "ALPHA", description: "Chief Admin", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1174350241714147349/1175346468148760607/FB_IMG_1699331108248.jpg?ex=656ae5b2&is=655870b2&hm=e73e554042ff0f2826f69859467acd94d97da6b3d0a394abb2110f1641655da1&"},
+    {name: "COSTA", description: "Chief Admin", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1174350241714147349/1175347872510791750/Screenshot_20231118-191131_Instagram.png?ex=656ae701&is=65587201&hm=8ec4dfd33b734bb67841195d7fedf726fa1f3d18768f31f05357484b5377e60f&"},
+    {name: "MAX", description: "Senior Management", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1173488210542874624/1175349997127086090/IMG_8521.png?ex=656ae8fc&is=655873fc&hm=90ae9e5daa31f4d326d6c8e6676c6cd9944a156195d3e5cfea3ede93220a3b74&"},
+    {name: "NIPUNA", description: "Senior Management", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1173488210542874624/1175351266965192737/IMG_8526.jpg?ex=656aea2b&is=6558752b&hm=3731888a426ae7db6c505c425fbd9ea8ba7fda90b00ae635e124c9b540a1da96&"},
+    {name: "SACHI", description: "Management", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1174350241714147349/1175344526827724810/Picsart_23-05-18_16-03-54-354.jpg?ex=656ae3e4&is=65586ee4&hm=126551b56f785cdd2fb590157a3a2222eb13bad9d65dfeba846775b07fd8bede&"},
+    {name: "BLACKA", description: "Staff Head", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1173488210542874624/1175349979779436575/IMG_8522.png?ex=656ae8f8&is=655873f8&hm=cc33fe6d1aa0bc1b8f50ff492236244ccebf00797f02f10bd28162af51e00313&"},
+    {name: "YANKY", description: "Staff Head", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1174350241714147349/1175350257429790761/Screenshot_20221013-120035_Gallery.jpg?ex=656ae93a&is=6558743a&hm=23886c0e433ede3c7a08d90027f66fc3324b6e4a672d5d630b4e90a26ba575e3&"},
+    {name: "PODDA", description: "Staff Head", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/1173488210542874624/1175349946090786846/IMG_8524.png?ex=656ae8f0&is=655873f0&hm=fed5a0c0c83b10520e9eae159818ccbc48bdbf8b4baa7391583772f3f7974573&"},
+    
+
+
+];
+
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: true},
+    {label: "Staff", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
