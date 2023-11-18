60987a62601e63f0df09440b7617521d194d64f1
Ashan Hannadige <151367115+SLESGT@users.noreply.github.com>
2023-11-18 16:45:52 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'https://discord.gg/NHq8bP3TVy'
+description 'Toxic-Loading'
+version '0.1'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+client_script 'client.lua'
+
+--loadscreen_manual_shutdown "yes"
+loadscreen 'index.html'
